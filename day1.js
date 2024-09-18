8799da2cf683722b0916d5f5be2b7ac285022a0d
Srilekha022 <srilekhamasam@gmail.com>
2024-09-18 12:06:28 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,29 @@
+//datatypes
+console.log("SRI"); //string
+console.log(20);    //number
+console.log(true);  //boolean
+console.log(false);
+
+
+//variables: containers which stores the data(var)
+
+//var name;      //variable declaration
+//name="sri";      //assigining
+
+var age=40;      //initialization
+var job="developer";
+var sal=9999;
+var sal=2222;
+var name="Srilekha";
+
+console.log(age);
+console.log(sal);
+console.log(job);
+console.log(movie); //not defined
+
+// var details{
+//name:"sri";
+//age:20;
+//}
+//console.log(.);      . -->all detalis
+
